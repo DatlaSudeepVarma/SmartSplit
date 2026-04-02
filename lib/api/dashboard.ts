7b8254a0bed9b9b5f1c284295943fd040d13d777
Datla Sudeep Varma <sudeep.varma.datla@gmail.com>
2026-04-02 14:33:15 +0530

    added db tables

--- lib/api/dashboard.ts
+++
@@ -1,6 +0,0 @@
-import { apiRequest } from './client';
-import { UserStats } from './types';
-
-export function getUserStats(_userId: string) {
-    return apiRequest<UserStats>('/me/stats', { method: 'GET' });
-}
